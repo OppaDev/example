615c34ead6e67e302757d724324649515ce39ba4
Leonardo <jlobando2@espe.edu.ec>
2024-05-29 08:28:58 -0500

    cambiar nombre

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    my_set = {"Hello World from Railway whit Flask!"}
+    my_set = {"Hello Leo Ða Vinci!"}
     return jsonify(list(my_set))
 
 
